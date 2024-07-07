9e62e6d53185a40aef5e2217f6250b8cecae4670
Alex Westphal <awestphal@onpointlearning.com>
2024-07-07 20:45:39 +1200

    Implemented color mode switcher.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
+
 import type { Metadata } from 'next'
+import React from 'react'
 
-import CssBaseline from '@mui/material/CssBaseline'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
-import { ThemeProvider } from '@mui/material/styles'
-
-import theme from '@/app/theme'
 
 import NavigationDrawer from '@/components/NavigationDrawer'
 
+import {UserPreferencesProvider} from './UserPreferences'
 
 export const metadata: Metadata = {
-  title: "Alex Westphal",
-  description: "Personal Website of Alex Westphal",
-};
+    title: "Alex Westphal",
+    description: "Personal Website of Alex Westphal",
+}
 
-export default function RootLayout(props: Readonly<{children: React.ReactNode }>) {
-  return <html lang="en">
-      <body>
-        <AppRouterCacheProvider>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <div style={{ display: 'flex' }}>
-                    <NavigationDrawer/>
-                    <main style={{ flexGrow: 1 }}>{props.children}</main>
-                </div>
+export default function RootLayout(props: Readonly<{ children: React.ReactNode }>) {
+
+    return <AppRouterCacheProvider>
+        <UserPreferencesProvider>
+            <html lang="en">
+                <body>
+                    <div style={{ display: 'flex' }}>
+                        <NavigationDrawer/>
+                        
+                        <main style={{ flexGrow: 1 }}>{props.children}</main>
+                    </div>
+                </body>
+            </html>
             
-            </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+        </UserPreferencesProvider>
+    </AppRouterCacheProvider>
 }
--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 
 import React from 'react'
@@ -8,17 +7,24 @@ import AppsIcon from '@mui/icons-material/Apps'
 import ArticleIcon from '@mui/icons-material/Article'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import DarkModeIcon from '@mui/icons-material/DarkModeOutlined'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LightModeIcon from '@mui/icons-material/LightMode'
 import PersonIcon from '@mui/icons-material/Person4'
+import RTLIcon from '@mui/icons-material/FormatTextdirectionRToLSharp'
 import ScienceIcon from '@mui/icons-material/Science'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightnessSharp'
+import TranslateIcon from '@mui/icons-material/TranslateSharp'
 
-import { Button, Divider, IconButton, List, ListItem, ListItemButton, listItemButtonClasses, ListItemIcon, listItemIconClasses, ListItemText, Paper } from '@mui/material'
+import { NoSsr } from '@mui/base'
+import { Box, Button, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Menu, MenuItem, Paper, Tooltip, listItemButtonClasses, listItemIconClasses } from '@mui/material'
+
+import { UserPreferences, useUserPreferences } from '@/app/UserPreferences'
+import { cls, createClasses, createIDs } from '@/util/cls'
 
 
-import { cls, createClasses } from '@/util/cls'
 
-
-
-const drawerWidth = 320;
+const DRAWER_WIDTH = 320;
 
 const navItems: { label: string, key: string, icon?: React.ReactNode }[] = [
     { label: 'Profile', key: 'profile', icon: <PersonIcon/> },
@@ -27,11 +33,12 @@ const navItems: { label: string, key: string, icon?: React.ReactNode }[] = [
     { label: 'Experiments', key: 'experiments', icon: <ScienceIcon/> },
 ]
 
-export const navigationDrawerClasses = createClasses('NavigationDrawer', ['closed', 'collapseControl', 'copyright', 'experienceControls', 'header', 'listItem', 'name', 'open', 'scrollable'])
+export const navigationDrawerClasses = createClasses('NavigationDrawer', ['closed', 'collapseControl', 'copyright', 'header', 'listItem', 'name', 'open', 'scrollable'])
+
 
 export default function NavigationDrawer() {
 
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(true)
 
     const handleDrawerOpen = () => {
         setOpen(true)
@@ -55,19 +62,17 @@ export default function NavigationDrawer() {
                 overflowX: 'hidden',
 
                 [`&.${classes.open}`]: {
-                    width: drawerWidth,
+                    width: DRAWER_WIDTH,
                     transition: theme.transitions.create('width', {
                         easing: theme.transitions.easing.sharp,
                         duration: theme.transitions.duration.enteringScreen,
                     }),
-                    overflowX: 'hidden',
                 },
                 [`&.${classes.closed}`]: {
                     transition: theme.transitions.create('width', {
                         easing: theme.transitions.easing.sharp,
                         duration: theme.transitions.duration.leavingScreen,
                     }),
-                    overflowX: 'hidden',
                     width: `calc(${theme.spacing(7)} + 1px)`,
                     [theme.breakpoints.up('sm')]: {
                         width: `calc(${theme.spacing(8)} + 1px)`,
@@ -77,7 +82,7 @@ export default function NavigationDrawer() {
 
             [`& .${classes.copyright}`]: {
                 padding: 2,
-                color: theme.palette.text.secondary,
+                color: 'text.secondary',
                 textAlign: 'center',
             },
             [`& .${classes.listItem}`]: {
@@ -90,7 +95,7 @@ export default function NavigationDrawer() {
                 },
                 [`& .${listItemIconClasses.root}`]: {
                     minWidth: 0,
-                    mr: open ? 3 : 'auto',
+                    [theme.direction == 'ltr' ? 'mr' : 'ml']: open ? 3 : 'auto',
                     justifyContent: 'center',
                 },
             },
@@ -117,13 +122,9 @@ export default function NavigationDrawer() {
                 component={NextLink}
                 href="/"
             >{open ? "Alex Westphal" : "AW"}</Button>
-            <div className={classes.experienceControls}>
-                <IconButton>
-
-                </IconButton>
-            </div>
         </div>
-        
+        <Divider/>
+        <ExperienceControls drawerOpen={open}/>
         <Divider/>
         <div className={classes.scrollable}>
             <List>
@@ -146,17 +147,142 @@ export default function NavigationDrawer() {
         </div>
         {open && <div className={classes.copyright}>© {new Date().getFullYear()} Alex Westphal.</div>}
         <Divider/>
-        <div className={classes.collapseControl}>
-            {open 
-            ? <Button sx={{ width: '100%' }}
-                startIcon={<ChevronLeftIcon/>}
-                onClick={handleDrawerClose}
-            
-            >Collapse Drawer</Button>
-            : <Button onClick={handleDrawerOpen}>
-                <ChevronRightIcon/>
-            </Button>
-        }
-        </div>
+        <NoSsr>
+            <div className={classes.collapseControl}>
+                {open 
+                    ? <Button sx={{ width: '100%' }}
+                        startIcon={<ChevronLeftIcon/>}
+                        onClick={handleDrawerClose}
+                    
+                    >Collapse Drawer</Button>
+                    : <Button onClick={handleDrawerOpen}>
+                        <ChevronRightIcon/>
+                    </Button>
+                }
+            </div>
+        </NoSsr>
     </Paper>
+}
+
+
+
+interface ExperienceControlsProps {
+    drawerOpen: boolean
+}
+
+const experienceControlsClasses = createClasses('ExperienceControls', [])
+
+const ExperienceControls: React.FC<ExperienceControlsProps> = ({drawerOpen}) => {
+
+    const userPreferences = useUserPreferences()
+    const [colorModeMenuAnchorEl, setColorModeMenuAnchorEl] = React.useState<HTMLElement | null>(null)
+
+    const classes = experienceControlsClasses
+    return <Box 
+        className={classes.root}
+        sx={{
+            [`&.${classes.root}`]: {
+                display: 'flex',
+                justifyContent: 'space-around',
+                marginY: 1,
+            },
+        }}
+    >
+        <ColorModeControl colorMode={userPreferences.colorMode} onChange={(value) => userPreferences.setColorMode(value)}/>
+        <IconButton>
+            <TranslateIcon/>
+        </IconButton>
+        <IconButton>
+            <RTLIcon/>
+        </IconButton>
+        <IconButton>
+            <GitHubIcon/>
+        </IconButton>
+        <Menu 
+            open={Boolean(colorModeMenuAnchorEl)}
+            anchorEl={colorModeMenuAnchorEl}
+        >
+
+        </Menu>
+    </Box>
+}
+
+interface ColorModeControlProps {
+    colorMode: UserPreferences.ColorMode
+    onChange: (colorMode: UserPreferences.ColorMode) => void
+}
+
+const colorModeControlIDS = createIDs('ColorModeControl', ['button', 'menu'])
+
+const ColorModeControl: React.FC<ColorModeControlProps> = ({ colorMode, onChange }) => {
+
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null)
+    const menuOpen = Boolean(anchorEl)
+
+    const handleOpenMenu = (ev: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(ev.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleClickMenuItem = (newColorMode: UserPreferences.ColorMode) => {
+        if(newColorMode != colorMode) onChange(newColorMode)
+        setAnchorEl(null)
+    }
+
+    return <>
+        <Tooltip title="Change Color Mode">
+            <IconButton
+                id={colorModeControlIDS.button}
+                aria-controls={menuOpen ? colorModeControlIDS.menu : undefined}
+                aria-haspopup="true"
+                aria-expanded={menuOpen ? 'true' : undefined}
+                onClick={handleOpenMenu}
+            >
+                <DarkModeIcon/>
+            </IconButton>
+        </Tooltip>
+        
+        <Menu
+            id={colorModeControlIDS.menu}
+            anchorEl={anchorEl}
+            open={menuOpen}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby': colorModeControlIDS.button,
+                disablePadding: true,
+                sx: { width: 200 }
+            }}
+        >
+            <ListSubheader>Color Mode</ListSubheader>
+            <MenuItem
+                selected={colorMode == 'light'}
+                onClick={() => handleClickMenuItem('light')}
+            >
+                <ListItemIcon>
+                    <LightModeIcon fontSize="small"/>
+                </ListItemIcon>
+                <ListItemText>Light</ListItemText>
+            </MenuItem>
+            <MenuItem
+                selected={colorMode == 'system'}
+                onClick={() => handleClickMenuItem('system')}
+            >
+                <ListItemIcon>
+                    <SettingsBrightnessIcon fontSize="small"/>
+                </ListItemIcon>
+                <ListItemText>System</ListItemText>
+            </MenuItem>
+            <MenuItem
+                selected={colorMode == 'dark'}
+                onClick={() => handleClickMenuItem('dark')}
+            >
+                <ListItemIcon>
+                    <DarkModeIcon fontSize="small"/>
+                </ListItemIcon>
+                <ListItemText>Dark</ListItemText>
+            </MenuItem>
+        </Menu>
+    </>
 }
